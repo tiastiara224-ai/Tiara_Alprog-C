e88b37af62f0ce86b191cd26199508241c59b40b
Tiara_Alprog-C <tiastiara224@gmail.com>
2025-10-30 21:22:07 +0800

    Add files via upload

---
+++ meet1.py
@@ -0,0 +1,5 @@
+print("Nama saya Baiq tiara nuringtias")
+print("Saya berasal dari kabupaten Lombok Tengah, kecamatan Praya Timur, Desa Marong")
+print("umur saya 18 tahun")
+print("Hobby saya menghayalkan masa depan")
+# azekkkkkkk
