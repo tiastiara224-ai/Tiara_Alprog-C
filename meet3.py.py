664b8a5b9dcbb3df746f23aedfdfadf754192476
Tiara_Alprog-C <tiastiara224@gmail.com>
2025-11-13 15:04:15 +0800

    Add files via upload

---
+++ meet3.py.py
@@ -0,0 +1,18 @@
+# tipe data dari sebuah nilai
+
+x = 10 # tipe data integer / bilangan bulat
+print("nila dari x =", x)
+
+print("tipe dari x =", type (x))
+
+y = 3.5 # tipe dari float / bilangan pecahan
+print("nilai dari y =", y)
+print("tipe dari y =", type (y))
+
+nama = "ayaa" # tipe dari string / bilangan berupa tulisan
+print("nilai dari nama =", nama )
+print("tipe dari nama =", type (nama))
+
+kebenaran = True # tipe data bolean
+print("nilai dari kebenaran =", kebenaran)
+print("tipe dari kebenaran =", type (kebenaran))
