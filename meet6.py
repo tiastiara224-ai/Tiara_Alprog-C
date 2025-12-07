c489fcbb077f8edb1bda70173ed556c0dd2bb8b1
Tiara_Alprog-C <tiastiara224@gmail.com>
2025-12-07 19:09:16 +0800

    Update temperature conversion program details

--- meet6.py
+++ meet6.py
@@ -1,8 +1,8 @@
 # operasi aritmatika
 # buatlah program konversi suhu
 
-# nama : Ummul Khairatin
-# nim : 25241087
+# nama : Baiq Tiara Nuringtias
+# nim : 25241086
 
 # masukkan suhu anda dalam celcius
 # masukkan suhu anda dalam fahrenheit
@@ -22,4 +22,5 @@ print ("suhu anda dalam reamur :" , reamur)
 # kelvin
 celcius = float(input("masukkan suhu anda dalam celcius:"))
 kelvin = celcius + 273
+
 print ("suhu anda dalam kelvin :" , kelvin)
